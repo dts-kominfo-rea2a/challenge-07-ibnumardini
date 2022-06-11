89b75ca59c9652c61a63668126f0029b289658e5
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-06-11 09:26:08 +0700

    feat: challenge answered

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (rating) => {
+  const theaterIxx = await promiseTheaterIXX();
+  const theaterVgc = await promiseTheaterVGC();
+
+  // merge films
+  const films = theaterIxx.concat(theaterVgc);
+
+  // count rating
+  const result = countRating(rating, films);
+
+  return result;
+};
+
+const countRating = (rating, films) => {
+  const filmsFiltered = films.filter((film) => film.hasil === rating);
+
+  return filmsFiltered.length;
+};
 
 module.exports = {
   promiseOutput,
